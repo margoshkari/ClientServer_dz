bd4684f59a9ff1b09d8d4e17a6e877f9d9e18311
Margo <margoshka.ri6@gmail.com>
2021-09-22 22:31:41 +0300

    added sending message to clients

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
         static ServerData serverData = new ServerData();
+        static bool isStartNewTask = true;
         static void Main(string[] args)
         {
             Console.WriteLine("Start server...");
@@ -18,25 +20,50 @@ namespace Server
                 serverData.socket.Bind(serverData.iPEndPoint);
                 serverData.socket.Listen(10);
 
-                Start();
+                Task.Factory.StartNew(() => Connect());
+
+                while(true)
+                {
+                    if(isStartNewTask)
+                    {
+                        Task.Factory.StartNew(() => SendMsg());
+                        isStartNewTask = false;
+                    }
+                }
+
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
-        static void Start()
+        static void Connect()
         {
             while (true)
             {
                 serverData.socketClient = serverData.socket.Accept();
+                serverData.socketClientList.Add(serverData.socketClient);
 
                 serverData.socketClient.Send(Encoding.Unicode.GetBytes("Welcome on server!"));
-
-
-                serverData.socketClient.Shutdown(SocketShutdown.Both);
-                serverData.socketClient.Close();
             }
+        }
+        static void SendMsg()
+        {
+            Console.WriteLine("Enter message or path:");
+            string msg = Console.ReadLine();
+            foreach (var item in serverData.socketClientList)
+            {
+                if (File.Exists(msg) && Path.GetFileName(msg).Contains(".txt") || Path.GetFileName(msg).Contains(".rtf"))
+                {
+                    item.Send(Encoding.Unicode.GetBytes(File.ReadAllText(msg)));
+                }
+                else
+                {
+                    item.Send(Encoding.Unicode.GetBytes(msg));
+                }
+            }
+            isStartNewTask = true;
         }
     }
 }
