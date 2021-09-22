271871af35989962686659f6943181005e10091d
Margo <margoshka.ri6@gmail.com>
2021-09-22 19:42:11 +0300

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static ServerData serverData = new ServerData();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start server...");
+            try
+            {
+                serverData.socket.Bind(serverData.iPEndPoint);
+                serverData.socket.Listen(10);
+
+                Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void Start()
+        {
+            while (true)
+            {
+                serverData.socketClient = serverData.socket.Accept();
+
+                serverData.socketClient.Send(Encoding.Unicode.GetBytes("Welcome on server!"));
+
+
+                serverData.socketClient.Shutdown(SocketShutdown.Both);
+                serverData.socketClient.Close();
+            }
+        }
+    }
+}
